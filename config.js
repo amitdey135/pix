0bf2d18872cf7e0e15d896ae269cf589a8b81b4b
amitdey135 <85150471+amitdey135@users.noreply.github.com>
2021-06-10 10:36:42 +0530

    Update config.js

--- config.js
+++ config.js
@@ -37,6 +37,6 @@ module.exports = {
   //Please go to https://developer.spotify.com/dashboard/
   Spotify: {
     ClientID: process.env.Spotify_ClientID || "5fcc2fd691cb4deb845299578b7e0ce8", //Spotify Client ID
-    ClientSecret: process.env.Spotify_ClientSecret || "", //Spotify Client Secret
+    ClientSecret: process.env.Spotify_ClientSecret || "1459e1e0b9d942d18dbcb1ac840ae2c4", //Spotify Client Secret
   },
 };
